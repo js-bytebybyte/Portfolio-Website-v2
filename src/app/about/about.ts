48f23907cef660c9070352125fa6d308c8fdd596
jolande <jolande.steenput@hotmail.com>
2026-03-23 14:46:41 +0100

    updating cv

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -24,7 +24,7 @@ export class About {
 
   events = EXPERIENCE;
 
-  // function to download the cv - pdf
+  // function to download the cv - pdf --> DEPRECATED
   downloadCv(): void {
     
     // create anchor element
